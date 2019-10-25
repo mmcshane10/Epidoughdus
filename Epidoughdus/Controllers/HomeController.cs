5d909f2964d1a7152e8098f6ec85565f88bad0a5
Mike <mmcshane10@gmail.com>
2019-10-25 14:21:28 -0700

    add search results view and route

--- Epidoughdus/Controllers/HomeController.cs
+++ Epidoughdus/Controllers/HomeController.cs
@@ -1,18 +1,38 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Epidoughdus.Models;
 
 namespace Epidoughdus.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly EpidoughdusContext _db;
+
+        public HomeController(EpidoughdusContext db)
+        {
+            _db = db;
+        }
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Search(string search)
+        {
+            search = search.ToLower();
+            List<Treat> model = _db.Treats.Include(treat => treat.Flavors).ToList();
+            List<Treat> newModel = new List<Treat> { };
+            for (int i = 0; i < model.Count; i++)
+            {
+                model[i].Name = model[i].Name.ToLower();
+                newModel.Add(model[i]);
+            }
+            return View("Search", newModel.Where(r => r.Name.Contains(search)).ToList());
         }
     }
 }
